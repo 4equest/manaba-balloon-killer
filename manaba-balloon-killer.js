a054b436eef9a942e6b04e363063b5b73648388c
4equest <107108812+4equest@users.noreply.github.com>
2023-12-13 22:22:08 +0900

    Update manaba-balloon-killer.js

--- manaba-balloon-killer.js
+++ manaba-balloon-killer.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         manaba balloon killer(public ver.)
+// @name         manaba balloon killer
 // @namespace    https://REPLACE ME/
 // @version      0.1
 // @description  manabaからバルーン(外部のURL押したときに出てくるやつ)を消します。　自己責任で利用してください。
 // @author       4equest
 // @match        https://REPLACE ME/*
 // @grant        none
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
@@ -14,27 +14,25 @@
 
     const ct_domain = "REPLACE ME"; // "ct.kansha.ac.jp" みたいにあなたが利用するmanabaのドメイン名をここに
 
-    console.log("[ballon killer] (=^・ω・^=)");
     manaba.balloon = null;
 
-    // すべてのaタグを取得
-    var aTags = document.getElementsByTagName("a");
-
-    for (var i = 0; i < aTags.length; i++) {
-        var aTag = aTags[i];
-
-        // hrefが"link_iframe_balloon?url="で始まり、onclickが"return manaba.linkballoon(event);"であるaタグを探す
-        if (aTag.href.startsWith("https://" + ct_domain + "/ct/link_iframe_balloon?url=") && aTag.getAttribute("onclick") === "return manaba.linkballoon(event);") {
-            console.log("[ballon killer] replace " + aTag);
-            // onclick属性を削除
-            aTag.removeAttribute("onclick");
-
-            // href属性の"link_iframe_balloon?url="以降の文字列をデコード
-            var decodedUrl = decodeURIComponent(aTag.href.split("https://" + ct_domain + "/ct/link_iframe_balloon?url=")[1]);
-
-            // デコードしたURLをaタグのhrefとして設定
-            aTag.href = decodedUrl;
-        }
+    const replaceLinks = () => {
+        console.log("[ballon killer] (=^・ω・^=)");
+        Array.from(
+            document.querySelectorAll('a[href*="link_iframe_balloon?url="]'),
+            el => {
+                el.onclick = void 0
+                el.target = '_blank'
+                const re = /link_iframe_balloon\?url=(.+)/
+                el.href = decodeURIComponent(el.href.match(re)[1])
+            }
+        )
     }
 
+    // 初回実行
+    replaceLinks();
+
+    // DOM変更を監視
+    const observer = new MutationObserver(replaceLinks);
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
